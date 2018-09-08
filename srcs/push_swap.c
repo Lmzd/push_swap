8c21e48d74f059e19d107f71f241e4ddfe39e1e3
Lmzd <mazeaud.louis@gmail.com>
2018-09-09 01:38:35 +0200

    Update the sort by tree and sort it with id

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/27 03:43:07 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/09/04 20:34:56 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/09/09 01:33:03 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,30 @@ void	ft_sort_three(t_stack *a, t_stack *b)
 {
 	if (ft_check_sort(a, b))
 		exit(0);
-	if (a->head->id - a->head->next->id == 1)
-		swap_a(a, b, 1);
-	if ((a->head->id - a->foot->id) == 2)
+	if ((a->head->id == 2 && a->foot->id == 1)
+		|| (a->head->id == 2 && a->foot->id == 0))
 		rotate_a(a, b, 1);
-	else if ((a->head->id - a->foot->id) == 1 ||
-	(a->head->id - a->foot->id) == -1)
+	else if ((a->head->id == 0 && a->foot->id == 1)
+		|| (a->head->id == 1 && a->foot->id == 0))
 		rotate_rev_a(a, b, 1);
+	else if (a->head->id == 1 && a->foot->id == 2)
+		swap_a(a, b, 1);
 	ft_sort_three(a, b);
+}
+
+void	ft_sort_all(t_stack *a, t_stack *b)
+{
+	if (ft_check_sort(a, b))
+		exit(0);
+	ft_sort_all(a, b);
 }
 
 void	ft_sort_stack(t_stack *a, t_stack *b)
 {
 	if (a->nb_val <= 3)
 		ft_sort_three(a, b);
-	// else if (a->nb_val <= 6)
-	// 	ft_sort_few(a, b);
-	// else
-	// 	ft_sort_all(a, b);
+	else
+		ft_sort_all(a, b);
 }
 
 int		main(int argc, char **argv)
