5d08876d24122f3615a85786df95c1022b08c785
Lmzd <mazeaud.louis@gmail.com>
2018-09-09 06:32:56 +0200

    change data to n in list struct

--- srcs/get_stack_params.c
+++ srcs/get_stack_params.c
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/06 19:53:22 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/09/09 02:44:34 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/09/09 04:49:45 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int		ft_get_min(t_lst *list)
 	int		min;
 
 	elem = list;
-	min = elem->data;
+	min = elem->n;
 	while (elem)
 	{
-		if (elem->data < min)
-			min = elem->data;
+		if (elem->n < min)
+			min = elem->n;
 		elem = elem->next;
 	}
 	return (min);
@@ -34,11 +34,11 @@ int		ft_get_max(t_lst *list)
 	int		max;
 
 	elem = list;
-	max = elem->data;
+	max = elem->n;
 	while (elem)
 	{
-		if (elem->data > max)
-			max = elem->data;
+		if (elem->n > max)
+			max = elem->n;
 		elem = elem->next;
 	}
 	return (max);
@@ -48,13 +48,15 @@ int		ft_get_med(t_lst *list, int nb_val)
 {
 	int		med;
 	t_lst	*elem;
+	int		odd;
 
 	elem = list;
-	med = (nb_val + 1) / 2;
+	odd = (nb_val % 2) ? 0 : 1;
+	med = (nb_val + odd) / 2;
 	while (elem)
 	{
 		if (elem->id == med)
-			return (elem->data);
+			return (elem->n);
 
 		elem = elem->next;
 	}
