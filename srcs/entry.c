7a56acf684cbab76d6cc3610ea5c73cfde62e12a
Lmzd <mazeaud.louis@gmail.com>
2018-09-11 23:32:19 +0200

    Update STDIN read for leaks

--- srcs/entry.c
+++ srcs/entry.c
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/04 07:02:23 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/09/04 07:34:49 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/09/11 23:26:03 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,25 @@ char	**read_entry(void)
 {
 	char	*line;
 	char	*result;
+	char	*get_line;
 	char	**entry;
 	int		ret;
 
-	result = ft_strnew(1);
 	ret = 1;
+	result = ft_strnew(0);
 	while (ret > 0)
 	{
 		ret = get_next_line(0, &line);
 		if (ret > 0)
 		{
-			result = ft_strjoin(result, line);
-			result = ft_strjoin(result, "\n");
+			get_line = ft_strjoin(result, line);
+			free(line);
+			free(result);
+			result = ft_strjoin(get_line, "\n");
+			free(get_line);
 		}
 	}
 	entry = ft_strsplit(result, '\n');
+	free(result);
 	return (entry);
 }
