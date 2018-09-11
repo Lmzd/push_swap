39c1160ba66be7811b6f52815b85242f7c728757
Lmzd <mazeaud.louis@gmail.com>
2018-09-11 23:31:31 +0200

    update Push_swap header for main push alog

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 17:27:32 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/09/10 13:58:55 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/09/11 23:25:11 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ typedef struct		s_stack
 {
 	t_lst			*head;
 	t_lst			*foot;
+
 	int				max;
 	int				min;
 	int				med;
@@ -38,12 +39,14 @@ typedef struct		s_brain
 {
 	int				index;
 	int				loop;
-	int				nb_val_a;
-	int				nb_val_b;
+	int				mov_1;
+	int				mov_2;
+	int				id;
 	int				min;
 	int				max;
 	int				med;
 	int				*tab;
+	int				iteration;
 }					t_brain;
 
 int					ft_check_argv(char **argv);
@@ -52,7 +55,7 @@ t_lst				*ft_create_lst(int *tab, int nb_val);
 t_stack				*ft_init_stack(void);
 t_stack				*ft_create_stack(t_lst *begin, int *tab, int nb_val);
 void				ft_init_stack_params(t_stack *stack);
-int					*normalization(int	*tab, int nb_val);
+extern int			*normalization(int	*tab, int nb_val);
 void				ft_print_stack(const char *title, t_stack *stack);
 char				**read_entry(void);
 int					check_entry(char **entry);
@@ -69,7 +72,15 @@ void				ft_sort_three_a(t_stack *a, t_stack *b, int sort_status);
 void				push_to_b(t_stack *a, t_stack *b, int iteration);
 void				push_to_a(t_stack *a, t_stack *b);
 void				push_all_to_b(t_brain *brain, t_stack *a, t_stack *b);
-void				push_all_to_a(t_stack *a, t_stack *b, int id);
+void				push_all_to_a(t_brain *brain, t_stack *a, t_stack *b);
 t_brain				*ft_init_brain(int *tab_s, t_stack *a);
+int					get_iteratoin_number(int nb);
+int					ft_is_ra(t_brain *brain, t_stack *a);
+int					ft_is_rra(t_brain *brain, t_stack *a);
+int					ft_is_rb(t_brain *brain, t_stack *b);
+int					ft_is_rrb(t_brain *brain, t_stack *b);
+void				free_list(t_brain *brain, t_stack *a, t_stack *b, int *tab);
+void				free_stack(t_stack *stack);
+void				free_entry(char **entry, int *nb);
 
 #endif
